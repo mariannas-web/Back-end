6c6fe5ad5a787be0c75f971f58b6e803d1988bb8
unknown <rlmclaughlin@yahoo.com>
2019-10-01 16:57:19 -0500

    added authentication/login and restricted middleware

--- auth/authentication.js
+++ auth/authentication.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const db = require('../data/dbConfig')
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Users = require('../helpers/users-model')
 
-const router = express.Router()
+const secrets = require('../config/secrets')
 
 
 router.get('/', (req, res) => {
@@ -9,6 +11,48 @@ router.get('/', (req, res) => {
 })
 
 
+router.post('/register', (req, res) => {
+    let user = req.body
+    let hash = bcrypt.hashSync(user.password, 10)
+    user.password = hash
+
+    Users.add(user).then(savedInfo => {
+        res.status(201).json(savedInfo)
+    })
+    .catch(error => { 
+        res.status(500).json(error)
+    })
+})
+
+
+router.post('/login', (req, res) => {
+    let {username, password} = req.body
+
+    Users.findBy({username})
+      .first()
+      .then(user => {
+          if(user && bcrypt.compareSync(password, user.password)){
+              const token = generateToken(user)
+              res.status(201).json({token})
+          } else {
+              res.status(401).json({message: "The credentials that you've provided are invalid"})
+          }
+      })
+      .catch(error => {
+          res.status(500).json(error)
+      })
+})
+
+
+function generateToken(user){
+    const payload = {
+        username: user.username
+    }
+    const options = {
+        expiresIn: '1d'
+    }
+    return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 
 module.exports = router
---
+++ config/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'hail unto thee, from the abodes of the day'
+}
