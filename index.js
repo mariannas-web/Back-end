f443f54398e9ff6b2b32ba136a5d236ed3b92f13
unknown <rlmclaughlin@yahoo.com>
2019-09-30 22:20:02 -0500

    added user migration, index.js, and connected server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server/server')
 
-PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 8000
 
 server.listen(PORT, () => {
     console.log("Server is currently running on port", PORT)
---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+const server = express()
+
+const cors = require('cors')
+const helmet = require('helmet')
+
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet()) 
+
+server.get('/', (req, res) => {
+    res.status(200).send("Mariannas Web Is Live")
+})
+
+
+module.exports = server
