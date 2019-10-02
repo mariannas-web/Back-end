c686adf8a7b3e5a6a63ecb6ce55f11ae28ff6816
unknown <rlmclaughlin@yahoo.com>
2019-10-01 20:25:39 -0500

    made updates to the database

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex')
 const dbConfig = require('../knexfile.js')
 
-const dbEnv = process.env.DB_ENV || 'production'
+const dbEnv = process.env.DB_ENV || 'development'
 
 module.exports = knex(dbConfig[dbEnv])
