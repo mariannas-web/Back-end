e17d32fb2a4df3c863c52bc73531aceab14968e6
unknown <rlmclaughlin@yahoo.com>
2019-10-05 01:32:14 -0500

    made revision

--- data/migrations/20190930214604_user.js
+++ data/migrations/20190930214604_user.js
@@ -2,9 +2,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable('user', table => {
       table.increments()
-      table.string('username', 128)
-      table.string('email', 128)
-      table.string('password', 128)
+      table.string('username')
+      table.string('email')
+      table.string('password')
   })
 };
 
