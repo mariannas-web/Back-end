44eb41935ec7b61626ad556d68515bf7fc79d377
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2019-10-23 00:14:28 -0500

    moved files to fix bugs

--- routers/newsFeeds.js
+++ routers/newsFeeds.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
-const undergroundFeed = require('../mint.json')
+const undergroundFeed = require('../scraper/json/mint.json')
 const hedge = require('../zerohedge.json')
 
 
 //-----------------Mint Press News-------------------------------//
 
-router.get('/mint', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json(undergroundFeed)
 })
 
