e96ec85a5127ba5efa7c6a0e78986c78ff75db2b
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2019-10-23 11:26:34 -0500

    made some minor changes

--- routers/newsFeeds.js
+++ routers/newsFeeds.js
@@ -9,19 +9,6 @@ router.get('/mint', (req, res) => {
     res.status(200).json(undergroundFeed)
 })
 
-//------------------Zero Hedge-----------------------------------//
-
-router.get('/zerohedge', (req, res) => {
-    res.status(200).json(hedge)
-})
-
-//-----------------the intercept---------------------------------//
-
-router.get('/intercept', (req, res) => {
-    res.status(200).json(mint)
-})
-
-//---------------------------------------------------------------//
 
 
 module.exports = router 
