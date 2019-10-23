094ed9fb39fc5e355c97f534b35f9d168489ab24
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2019-10-23 11:51:02 -0500

    made change that was potentially causing the bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server/server')
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 7000
 
 server.listen(PORT, () => {
     console.log("Server is currently running on port", PORT)
--- routers/newsFeeds.js
+++ routers/newsFeeds.js
@@ -1,12 +1,11 @@
 const router = require('express').Router()
 const undergroundFeed = require('../mint.json')
-const hedge = require('../zerohedge.json')
 
 
 //-----------------Mint Press News-------------------------------//
 
 router.get('/mint', (req, res) => {
-    res.status(200).json({cat: 'james'})
+    res.status(200).json(undergroundFeed)
 })
 
 
